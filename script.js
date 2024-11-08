226a5f7c9760c106cbdc8531b56c251bddf34123
aldaydev <rafaldayparejo@gmail.com>
2024-11-08 15:09:02 +0100

    Haciendo ejercicio Quizz - mañana 20241108

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
 //Iniciar el QUIZZ
 function start(){
     //Recogemos las opciones para el QUIZZ
-    const catergory = document.getElementById('category');
-    const difficulty = document.getElementById('difficulty');
+    const catergory = document.getElementById('category').value;
+    const difficulty = document.getElementById('difficulty').value;
 
-    //Creamos la URL con las opciones
-    let getUrl = 'https://opentdb.com/api.php?amount=10' + catergory.value + difficulty.value;
+    //Creamos la URL a partir de las opciones
+    let getUrl = 'https://opentdb.com/api.php?amount=10' + catergory + difficulty;
 
     //Solicitamos los datos
     getQuestions(getUrl);
@@ -14,25 +13,30 @@ function start(){
 
 //Llamar a la API
 async function getQuestions(url){
+    //Llamar y descodificar API
     let response = await fetch(url);
     let questions = await response.json();
-    console.log(questions);
+
+    //Recopilar data de la API
     getQuestionsData(questions.results);
+
+    //Recogemos la sección inicial y la oculamos
     let initialSection = document.querySelector('.initialSection');
     hideElement(initialSection);
+
+    //Disponer la primera pregunta
     setQuestion();
 }
 
 //Recopilar la data de las preguntas
 function getQuestionsData(questions){
     questions.forEach((q, i) => {
-        let clave = i;
 
         let answers = q.incorrect_answers;
         answers.push(q.correct_answer);
         answers = answers.sort(() => Math.random() - 0.5);
 
-        localStorage[clave] = JSON.stringify({
+        localStorage[i] = JSON.stringify({
             question: q.question,
             answers: answers,
             correct: q.correct_answer
@@ -51,22 +55,72 @@ function hideElement(element){
     element.setAttribute('class', 'hidden');
 }
 
-//Disponer pregunta
+//Disponer la pregunta
 function setQuestion(){
-    
-    let questionSection = document.createElement('section');
-    questionSection.setAttribute('class', 'questionSection');
 
+    //Modificar subtítulo con la pregunta
     let subheader = document.getElementById('subheader');
     let currentQuestion = JSON.parse(localStorage.count).responsed;
-    subheader.textContent = JSON.parse(localStorage[currentQuestion]).question;
-    console.log(currentQuestion);
+    subheader.innerHTML = JSON.parse(localStorage[currentQuestion]).question.toUpperCase();
 
+    //Conformar un article con las respuestas y sus radios
+    let filledArticle = setAnswers(currentQuestion);
 
+    //Crear el botón de enviar respuesta
+    let responseBtn= document.createElement('button');
+    responseBtn.setAttribute('id', 'responseBtn');
+    responseBtn.setAttribute('onclick', 'sendResponse()');
+    responseBtn.innerText = 'SEND ANSWER';
+
+    //Crear sección con cada pregunta y meter el article
+    let questionSection = document.createElement('section');
+    questionSection.setAttribute('class', 'questionSection');
+    questionSection.appendChild(filledArticle);
+    questionSection.appendChild(responseBtn);
+
+    //Meter la section dentro del main
     let main = document.querySelector('main');
     main.appendChild(questionSection);
-
     
+}
+
+//Conformar un article con las respuestas y sus radios
+function setAnswers(current){
+    //Crear artículo donde irán las respuestas
+    let article = document.createElement('article');
+    //Seleccionamos las respuestas actuales
+    let currentAnswers = JSON.parse(localStorage[current]).answers;
+    //Recorrer respuestas y disponerlas
+    currentAnswers.forEach(currentAnswer => {
+        //Crear input de cada respuesta
+        let input = document.createElement('input');
+        input.setAttribute('type', 'radio');
+        input.setAttribute('name', 'answer');
+        input.setAttribute('class', 'radio');
+        //Crear texto de cada respuesta
+        let answerText = document.createElement('h3');
+        answerText.innerHTML = currentAnswer;
+        //Crear labbel y meter input + texto
+        let labbel = document.createElement('labbel');
+        labbel.setAttribute('class', 'answerLabbel')
+        labbel.appendChild(input);
+        labbel.appendChild(answerText);
+        //Meter cada respuesta en el article
+        article.setAttribute('class', 'answers');
+        article.appendChild(labbel);
+
+    })
+
+    return article;
+
+}
+
+function sendResponse(){
+    alert("Respuesta enviada");
+    let inputResponse = document.querySelector('input[name="answer"]:checked');
+    let textResponse = inputResponse.nextElementSibling.textContent;
+    console.log(textResponse);
+
 }
 
 // https://opentdb.com/api_category.php
